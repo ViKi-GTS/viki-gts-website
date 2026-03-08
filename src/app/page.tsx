17b144dfd9b8bd9b415264b7e26ece28fe4c4acd
ViKi Global TechSolutions <admin@viki-gts.com>
2026-03-08 15:49:19 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
                 <ul className="nav">
                   <li><a className="nav-link active" href="/">Home</a></li>
                   <li><a href="#about">About</a></li>
-                  <li><a href="#services">Services</a></li>
+                  <li><a href="/services">Services</a></li>
                   <li><a href="#projects">Case Studies</a></li>
                   <li><a href="#contact">Contact</a></li>
                 </ul>
